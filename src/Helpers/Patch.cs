1571a48b1a6b0a8b58f2cbb19d38ca79fb81e20b
WarperSan <leumas.ecole@gmail.com>
2025-09-24 14:16:36 -0400

    Simplified UseHarmony

---
+++ src/Helpers/Patch.cs
@@ -0,0 +1,52 @@
+#if UseHarmony
+using HarmonyLib;
+#endif
+
+namespace UnityModTemplate.Helpers;
+
+/// <summary>
+///     Class helping for patching stuff
+/// </summary>
+internal static class Patch
+{
+#if UseHarmony
+    private static Harmony? _harmony;
+#endif
+
+    /// <summary>
+    ///     Applies every patch needed by this mod
+    /// </summary>
+    public static void ApplyAll()
+    {
+#if UseHarmony
+        if (_harmony != null)
+        {
+            Log.Debug("Unpatching the existing harmony instance.");
+            RevertAll();
+        }
+        
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        _harmony.PatchAll(typeof(Patches.Template_Patches));
+
+#endif
+        Log.Debug("All patches applied.");
+    }
+
+    /// <summary>
+    ///     Reverts every patch applied by this mod
+    /// </summary>
+    public static void RevertAll()
+    {
+#if UseHarmony
+        if (_harmony == null)
+            return;
+        
+        _harmony.UnpatchSelf();
+
+        _harmony = null;
+
+#endif
+        Log.Debug("All patches reverted.");
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,58 +1,21 @@
 ﻿using BepInEx;
-#if UseHarmony
-using HarmonyLib;
-#endif
 using Log = UnityModTemplate.Helpers.Log;
+using Patch = UnityModTemplate.Helpers.Patch;
 
 namespace UnityModTemplate;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+internal class Plugin : BaseUnityPlugin
 {
     private void Awake()
     {
-#if UseLogger
-        Log.SetLogger(Logger);
-
-#endif
-#if UseHarmony
-        Patch();
-
-#endif
+        Patch.ApplyAll();
         Log.Info($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
     }
 
     private void OnDestroy()
     {
-#if UseHarmony
-        Unpatch();
-
-#endif
+        Patch.RevertAll();
         Log.Info($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has unloaded!");
     }
-#if UseHarmony
-
-    #region Harmony
-
-    private Harmony? Harmony;
-
-    private void Patch()
-    {
-        Harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-        
-        // Harmony.PatchAll(typeof(Patches.Template_Patches));
-
-        Log.Debug("Plugin patched!");
-    }
-
-    private void Unpatch()
-    {
-        Harmony?.UnpatchSelf();
-        Harmony = null;
-        
-        Log.Debug("Plugin unpatched!");
-    }
-
-    #endregion
-#endif
 }
